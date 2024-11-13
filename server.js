1b8d6bdb978de4b5a1835d87a198925da2181b0d
Nemo <76892046+NemoJohnFynz@users.noreply.github.com>
2024-11-13 09:36:13 +0700

    config server

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv/config';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(process.env.PORT, () => `Server running on port ${port} 🔥`);
+console.log(`Server running on port ${port} 🔥`);
